9acf454611c3c8219ab595b6442b5a1388a51d48
Leticia <leticiasantana0607@gmail.com>
2025-09-13 04:22:03 -0300

    inserindo delimitação da bolinha

--- HACKING THE SYSTEM/HACKING THE SYSTEM.c
+++ HACKING THE SYSTEM/HACKING THE SYSTEM.c
@@ -14,7 +14,7 @@ int main() {
 
 
     ALLEGRO_DISPLAY* janela = al_create_display(1280, 720); /// criação da janela
-    al_set_window_position(janela, 200, 200); // posição incial da janela
+    al_set_window_position(janela, 200, 200); // posição inicial da janela
 
     ALLEGRO_FONT* font = al_create_builtin_font();
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0); // definindo "FPS"
@@ -56,6 +56,30 @@ int main() {
         al_draw_filled_circle(pos_x1, pos_Y2, 50, al_map_rgb(124, 200, 50));
         al_draw_text(font, al_map_rgb(0, 0, 0), 15, 20, 0, "Hackin the Sistem\n testando ideia de livro\n para que consiga passar a mensagem");//colocando texto na tela
         al_flip_display();
+
+
+        //delimitando bolinha ao tamanho da tela
+        if (pos_x >= 1280 - 25) {
+            pos_x = pos_x - 40;
+        }
+        else if (pos_x <=0 + 25) {
+            pos_x = pos_x + 40;
+        }
+        else if (pos_y >= 720 - 25) {
+            pos_y = pos_y - 40;
+        }
+        else if (pos_y <= 0 + 25) {
+			pos_y = pos_y + 40;
+
+        }
+
+        //criando a entrada de dados a partir de uma condição
+        if (pos_x == pos_x1 && pos_y == pos_Y2) {
+            int resultado = 2;   
+            al_draw_text(font,al_map_rgb(3, 0, 0), 400, 360, 0, "Descubra para que valor de X obteremos a saída y=4 se 2X-2 = 4");
+            al_flip_display();
+
+        }
     }
 
     // destruindo eventos
