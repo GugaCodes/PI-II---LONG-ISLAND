9459f8657ef517d9ce129469e65fa324a678a21c
Gustavo-Oliveira-max <gsilvaoliveira642@gmail.com>
2025-09-16 18:16:01 -0300

    Tela Inicial

--- HACKING THE SYSTEM/Telas.c
+++ HACKING THE SYSTEM/Telas.c
@@ -2,12 +2,13 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/keyboard.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
 
 int main() {
 
     //criando uma váriavel de controle de telas
 
-    int tela = 2;
+    int tela = 0;
 
     float pos_x = 150, pos_y = 450, pos_x1 = 450, pos_Y2 = 450;// posição da bolinha
 
@@ -17,6 +18,7 @@ int main() {
     al_init_primitives_addon();
     al_install_keyboard();
     al_install_mouse();
+    al_init_image_addon();
 
 
     ALLEGRO_DISPLAY* janela = al_create_display(1280, 720); /// criação da janela
@@ -24,6 +26,7 @@ int main() {
 
     ALLEGRO_FONT* font = al_create_builtin_font();
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0); // definindo "FPS"
+    ALLEGRO_BITMAP* tela_inicial = al_load_bitmap("./TELA_INICIAL.png");
 
     ALLEGRO_EVENT_QUEUE* fila_eventos = al_create_event_queue(); // criação da fila de eventos
     al_register_event_source(fila_eventos, al_get_display_event_source(janela));
@@ -64,11 +67,7 @@ int main() {
         }
 
         if (tela == 0) { // tela inicial do jogo
-
-        al_clear_to_color(al_map_rgb(125, 125, 125));// colocando a cor da tela
-        al_draw_filled_circle(pos_x, pos_y, 50, al_map_rgb(0, 0, 255));// desenhando bolinha 1
-        al_draw_text(font, al_map_rgb(0, 0, 0), 15, 20, 0, "Hackin the Sistem\n testando ideia de livro\n para que consiga passar a mensagem");//colocando texto na tela
-
+            al_draw_bitmap(tela_inicial, 0, 0, 0);
         }
         if (tela == 2) { // tela 2 
            al_clear_to_color(al_map_rgb(0, 125, 125));// colocando a cor da tela
@@ -85,6 +84,7 @@ int main() {
     al_destroy_font(font);
     al_destroy_display(janela);
     al_destroy_event_queue(fila_eventos);
+    al_destroy_bitmap(tela_inicial);
 
     return 0;
 }
